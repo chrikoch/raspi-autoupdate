a2dfadb344bcc5d8102f62bc45d6a0056693d46a
Christian Koch <chri.koch@gmail.com>
2018-10-15 21:02:12 +0200

    passing configfile as parameter

--- update.sh
+++ update.sh
@@ -1,5 +1,18 @@
 #/bin/bash
 
-source config
+#set default
+configfile="config"
+
+
+while getopts "c:" optname
+do
+  case "$optname" in
+    "c")
+      configfile=$OPTARG
+      ;;
+  esac
+done
+
+source ${configfile}
 
 curl --header "Access-Token: ${PUSHBULLET_TOKEN}" --header 'Content-Type: application/json' -d "{\"type\":\"note\",\"device_iden\":\"${PUSHBULLET_DEVICE}\",\"title\":\"abc\",\"body\":\"Inhalt\"}" https://api.pushbullet.com/v2/pushes
